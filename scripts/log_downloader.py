0e7a1e5709197438800e0f15bba91558fc11809e
alexklimaj <alex@arkelectron.com>
2024-01-07 06:45:22 +0000

    can't get new number of log enteries when logging starts

--- scripts/log_downloader.py
+++ scripts/log_downloader.py
@@ -21,31 +21,42 @@ async def run():
             print(f"-- Connected to drone!")
             break
 
-    print("Waiting for log entries...")
+    # print("Waiting for log entries...")
 
-    entries = await get_entries(drone)
-    for entry in entries:
-        date_without_colon = entry.date.replace(":", "-")
-        filename = f"/home/{os.getenv('USER')}/Documents/logs/log-{date_without_colon}.ulog"
-        if not os.path.exists(filename):
-            await download_log(drone, entry, filename)
-        elif os.path.exists(filename) and os.path.getsize(filename) != entry.size_bytes:
-            print(f"Log {entry.id} from {entry.date} is not complete, redownloading...")
-            print(f"Local filesize:  {os.path.getsize(filename)}")
-            print(f"Remote filesize: {entry.size_bytes}")
-            os.remove(filename)
-            await download_log(drone, entry, filename)
-    
+    # entries = await get_entries(drone)
+    # for entry in entries:
+    #     date_without_colon = entry.date.replace(":", "-")
+    #     filename = f"/home/{os.getenv('USER')}/Documents/logs/log-{date_without_colon}.ulog"
+    #     if not os.path.exists(filename):
+    #         await download_log(drone, entry, filename)
+    #     elif os.path.exists(filename) and ((abs(os.path.getsize(filename) - entry.size_bytes) > 300000)):
+    #         print(f"Log {entry.id} from {entry.date} is not complete, redownloading...")
+    #         print(f"Local filesize:  {os.path.getsize(filename)}")
+    #         print(f"Remote filesize: {entry.size_bytes}")
+    #         os.remove(filename)
+    #         await download_log(drone, entry, filename)
+
+    last_entries = await drone.log_files.get_entries()
+    print(f"Number of entries: {len(last_entries)}")
+
     print("Waiting for new logs...")
 
-    # while True:
-    #     await asyncio.sleep(1)
-    #     entries = await get_entries(drone)
-    #     for entry in entries:
-    #         date_without_colon = entry.date.replace(":", "-")
-    #         filename = f"/home/{os.getenv('USER')}/Documents/logs/log-{date_without_colon}.ulog"
-    #         if not os.path.exists(filename):
-    #             await download_log(drone, entry, filename)
+    while True:
+        await asyncio.sleep(5)
+        entries = await drone.log_files.get_entries()
+        print(f"Number of entries: {len(entries)}")
+        
+        if entries[-1] != last_entries[-1]:
+
+            # get the latest entry
+            latest_entry = entries[-1]
+            print(f"New log {latest_entry.id} from {latest_entry.date} with size {latest_entry.size_bytes} bytes")
+            date_without_colon = latest_entry.date.replace(":", "-")
+            filename = f"/home/{os.getenv('USER')}/Documents/logs/log-{date_without_colon}.ulog"
+            if not os.path.exists(filename):
+                await download_log(drone, latest_entry, filename)
+
+            last_entries = entries
 
 
 async def download_log(drone, entry, filename):
